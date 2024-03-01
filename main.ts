864e4e3f7f64db10f4bcee1287f1f209f63cff6a
013Meta <rudydube@protonmail.com>
2024-03-01 23:28:39 +0100

    deepgram-voice-transcription

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, addIcon, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { createClient } from '@deepgram/sdk';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,25 +11,23 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+const RECORDING_STARTED_NOTICE = "I'm listening...";
+const RECORDING_STOPPED_NOTICE = "Stopping recording...";
+const ERROR_ACCESS_MICROPHONE = 'Error accessing the microphone. Please ensure you have given access.';
+
+export default class Deepscribe extends Plugin {
 	settings: MyPluginSettings;
+    mediaRecorder: MediaRecorder | null = null; // Add a class property to hold the MediaRecorder instance
+    isRecording: boolean = false; // Add a flag to track recording state
+    audioChunks: BlobPart[] = []; // Add a property to hold audio chunks
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.setupRibbonIcon();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.setupStatusBar();
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -36,7 +35,7 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -45,38 +44,100 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
 						new SampleModal(this.app).open();
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
+
+    setupRibbonIcon() {
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Transcription', this.toggleRecording.bind(this));
+        ribbonIconEl.addClass('my-plugin-ribbon-class');
+    }
+
+    setupStatusBar() {
+        const statusBarItemEl = this.addStatusBarItem();
+        statusBarItemEl.setText('Status Bar Text');
+    }
+
+    async toggleRecording() {
+        if (this.isRecording) {
+            this.stopRecording();
+            new Notice(RECORDING_STOPPED_NOTICE);
+        } else {
+            new Notice(RECORDING_STARTED_NOTICE);
+            await this.startRecording();
+        }
+    }
+
+    async startRecording() {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            this.handleRecording(stream);
+            this.isRecording = true;
+        } catch (error) {
+            console.error('Error accessing the microphone', error);
+            new Notice(ERROR_ACCESS_MICROPHONE);
+        }
+    }
+
+    handleRecording(stream: MediaStream) {
+        this.mediaRecorder = new MediaRecorder(stream);
+        this.audioChunks = [];
+
+        this.mediaRecorder.ondataavailable = this.handleDataAvailable.bind(this);
+
+        this.mediaRecorder.onstop = async () => {
+            const audioBlob = new Blob(this.audioChunks, { type: 'audio/wav' });
+            this.audioChunks = [];
+            this.isRecording = false;
+
+            const fileName = `Recording ${new Date().toISOString().replace(/[:.]/g, '')}.wav`;
+            const filePath = `/Audio/${fileName}`;
+            await this.app.vault.createBinary(filePath, await audioBlob.arrayBuffer());
+
+            const noteName = `Note with ${fileName}`;
+            const noteContent = `# Audio Note\n\n![[${filePath}]]`;
+            await this.app.vault.create(noteName + '.md', noteContent);
+        };
+
+        this.mediaRecorder.start();
+    }
+
+    handleDataAvailable(event: MediaRecorderDataAvailableEvent) {
+        this.audioChunks.push(event.data);
+    }
+
+    stopRecording() {
+        if (this.mediaRecorder && this.isRecording) {
+            this.mediaRecorder.stop();
+            this.isRecording = false;
+
+            if (this.mediaRecorder.stream && this.mediaRecorder.stream.getTracks) {
+                this.mediaRecorder.stream.getTracks().forEach(track => track.stop());
+            }
+
+            new Notice("Recording stopped.");
+        }
+    }
 
 	onunload() {
 
@@ -88,6 +149,23 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async transcribeAudio(filePath: string) {
+	  const deepgram = createClient(process.env.DG_API_KEY || '')
+	  const dgConnection = deepgram.listen.live({ model: "nova" });
+
+	  dgConnection.on('open', () => {
+	    console.log('Connection opened.');
+
+	    dgConnection.on('transcriptReceived', (data) => {
+	      console.log(data);
+	    });
+	  });
+
+	  dgConnection.on('close', () => {
+	    console.log('Connection closed.');
+	  });
 	}
 }
 
@@ -108,9 +186,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: Deepscribe;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Deepscribe) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
